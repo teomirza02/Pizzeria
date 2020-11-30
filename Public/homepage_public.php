9bf613c9545c846f076a4f16a9f5bea74ff07a2a
teomirza02 <toader.mirza.studenti@isii.it>
2020-11-30 20:06:24 +0100

    Create homepage_public.php

---
+++ Public/homepage_public.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Stylesheet.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>homepage_public</title>
+</head>
+<body>
+<div>
+    <h1 class = "title">Pizzeria Il Diavolo</h1>
+</div>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li><a href="homepage_public.php">Home</a></li>
+      <li><a href="menu.php">Menu </a></li>
+      <li><a href="contatti.php">Contatti</a></li>
+      <li><a href="recensioni_public.php">Recensioni</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="../index.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+<div class= "part2 backimg">
+  <div class="aboutus">
+    <h2>Chi siamo?</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    Integer a turpis suscipit, eleifend dui ac, sodales leo. Etiam sodales facilisis elit, eget tincidunt ante semper a. Nunc fermentum sed dui ut commodo. 
+    Nunc vitae suscipit urna, eget rhoncus nisl. Pellentesque et sem eu enim blandit fringilla in a purus. Phasellus dignissim pellentesque urna, in maximus velit consectetur ut. 
+    Donec gravida vel tortor eu mattis. Curabitur convallis nulla eget hendrerit placerat. 
+    Donec augue sem, venenatis et massa at, tincidunt feugiat mauris. Suspendisse ligula arcu, volutpat sed enim non, lacinia semper ligula.
+    </p>
+  </div>
+  <div class="aboutus">
+    <h2>La nostra cucina</h2>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    Integer a turpis suscipit, eleifend dui ac, sodales leo. Etiam sodales facilisis elit, eget tincidunt ante semper a.
+    Nunc fermentum sed dui ut commodo. 
+    </p>
+  </div>
+</div>
+<footer class="endfooter">
+  Copyright &copy; 2020 Mirza Toader, All Rights Reserved.
+</footer>
+
+</body>
+</html>
