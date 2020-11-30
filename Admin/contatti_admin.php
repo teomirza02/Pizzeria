9db6e3a90d7bd40ff2cb5cfc670ac6ad0f74b6d0
teomirza02 <toader.mirza.studenti@isii.it>
2020-11-30 20:05:22 +0100

    Create contatti_admin.php

---
+++ Admin/contatti_admin.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Stylesheet_admin.css">
+    <link rel="stylesheet" href="Stylesheet_contatti_admin.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>contatti_admin</title>
+</head>
+<body>
+<div>
+    <h1 class = "title">Pizzeria Il Diavolo</h1>
+</div>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li><a href="homepage_admin.php">Home</a></li>
+      <li><a href="menu_admin.php">Menu </a></li>
+      <li><a href="contatti_admin.php">Contatti</a></li>
+      <li><a href="recensioni_admin.php">Recensioni</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+
+    </ul>
+  </div>
+</nav>
+
+<div class="backimgcontatti contdiv">
+    <h1>Dove ci troviamo?</h1>
+    <p>
+        
+        <p>Via: Giuseppe gariabaldi n.18</p>
+        <p>Provincia: Piacenza</p>
+        <p>Italia</p>
+        
+    </p>
+    <h1>Come contattarci</h1>
+    <p>
+        
+        <p>Telefono: 333 222 1111</p>
+        <p>Email: email.fasulla@gmail.com</p>
+            
+        
+    </p>
+    <h1>Orario</h1>
+    <p>
+        <p>Lun - Mar - Mer - Ven:       08:00 - 23:00</p>
+        <p>Giov:        Chiuso</p>
+        <p>Sab - Dom:       08:00 - 00:30</p>
+    </p>
+
+
+</div>
+
+<footer class="endfooter">
+  Copyright &copy; 2020 Mirza Toader, All Rights Reserved.
+</footer>
+
+</body>
+</html>
